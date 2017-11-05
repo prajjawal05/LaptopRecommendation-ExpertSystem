4f5c26058f26109e67c5530610b85ccd3f55c3f4
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2017-11-06 03:32:09 +0530

    Update rules.pl

--- rules.pl
+++ rules.pl
@@ -12,3 +12,16 @@ isdev(X):-
     screen_size(X,S),S>14,
     (processor_brand(X,intel),(processor_model(X,"i5");processor_model(X,"i3"))),
     gc_size(X,G),G>=1.
+    
+isclerk(X):-
+    ram(X,R),R>=2,
+    hd_size(X,H),H>=512,
+    screen_size(X,S),S>12,
+    processor_brand(X,intel),
+    os(X,"windows").
+
+ishome(X):-
+    ram(X,R),R>=2,
+    hd_size(X,H),H>=512,
+    screen_size(X,S),S>13,
+    os(X,"windows").
