fccb5cd80ac465b45dd6aa7df35cc2663eb0135a
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2017-11-12 01:41:16 +0530

    Add files via upload

--- expert.pl
+++ expert.pl
@@ -1,32 +1,36 @@
 printlaptop(H):-
     brand(H,A),
     model(H,B),
-    write(H),write(' '),
+    price(H,R),
+    write(H),write(' -> '),
     write(A), write(' '),
-    write(B),nl.
+    write(B),write(' (Rs.'),
+    write(R),write(')'),nl.
 
 check([],_,[]).
+
 check([H|T],S,[H|Result]) :-
     S=:=1,
-    printlaptop(H),
     isgaming(H),
+	printlaptop(H),
     check(T,S,Result).
 
 check([H|T],S,[H|Result]) :-
     S=:=2,
-    printlaptop(H),
     isdev(H),
-    check(T,S,P,Result).
+    printlaptop(H),
+    check(T,S,Result).
+
 check([H|T],S,[H|Result]) :-
     S=:=3,
-    printlaptop(H),
     isclerk(H),
+    printlaptop(H),
     check(T,S,Result).
 
 check([H|T],S,[H|Result]) :-
     S=:=4,
-    printlaptop(H),
     ishome(H),
+    printlaptop(H),
     check(T,S,Result).
 
 check([_|T],S,Result) :-
@@ -46,4 +50,4 @@ test() :-
     input(P,L),
 	bubble_sort(L,X),
     %numlist(1,168,L),
-	check(X,B,M).
+	check(X,B,_).
--- rules.pl
+++ rules.pl
@@ -1,38 +1,40 @@
+:- discontiguous(chik/3).
+
 isgaming(X):-
     ram(X,R),R>=8,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>15,
-    (processor_brand(X,intel),(processor_model(X,"i5");processor_model(X,"i3");processor_model(X,"i7")));processor_brand(X,amd),
+    ((processor_brand(X,intel),(processor_model(X,'i5');processor_model(X,'i3');processor_model(X,'i7')));processor_brand(X,'amd')),
     gc_size(X,G),G>=2,
-    os(X,"windows").
+    os(X,'windows').
 
 isdev(X):-
     ram(X,R),R>=8,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>14,
     clock_speed(X,C),C>=2.3,
-    (processor_brand(X,intel),(processor_model(X,"i5");processor_model(X,"i3"))),
+    (processor_brand(X,'intel'),(processor_model(X,'i5');processor_model(X,'i3'))),
     gc_size(X,G),G>=1.
 
 isclerk(X):-
     ram(X,R),R>=2,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>12,
-    processor_brand(X,intel),
-    os(X,"windows").
+    processor_brand(X,'intel'),
+    os(X,'windows').
 
 ishome(X):-
     ram(X,R),R>=4,
     hd_size(X,H),H>=512,
     screen_size(X,S),S>13,
-    os(X,"windows");os(X,"mac").
+    os(X,'windows');os(X,'mac').
 
 compare_hd_type(P,Q):-
     A = 1, B = 2, B>A,
     hd_type(P,R),
     hd_type(Q,S),
     X = R, Y= S,
-    (X = "ssd", Y = "hdd").
+    (X = 'ssd', Y = 'hdd').
 
 compare_gc_size(P,Q):-
     gc_size(P,R),
@@ -64,44 +66,39 @@ compare_screen_size(P,Q):-
     screen_size(Q,S),
     X = R, Y= S,
     X>=Y.
-	
-mycompare(Delta,P,Q):-
-		screen_size(P,R),
-		screen_size(Q,S),
-        compare(>,P,Q).
-	
+
 compare_processors_brand(P,Q):-
     A = 1, B = 2, B>A,
     processor_brand(P,R),
     processor_brand(Q,S),
     X = R, Y= S,
-    ((X = "amd", Y = "amd");
-	(X = "intel" , Y = "amd");
-	(X = "intel" , Y = "intel")).
+    ((X = 'amd', Y = 'amd');
+	(X = 'intel' , Y = 'amd');
+	(X = 'intel' , Y = 'intel')).
 
 compare_graphics(P,Q):-
     A = 1, B = 2, B>A,
     gc_brand(P,R),
     gc_brand(Q,S),
     X = R, Y= S,
-    ((X = "nvidia", Y = "amd");
-	(X = "nvidia" , Y = "intel");
-	(X = "intel" , Y = "intel");
-	(X = "nvidia" , Y = "nvidia");
-	(X = "amd" , Y = "amd");
-    (X = "amd", Y = "intel")).
+    ((X = 'nvidia', Y = 'amd');
+	(X = 'nvidia' , Y = 'intel');
+	(X = 'intel' , Y = 'intel');
+	(X = 'nvidia' , Y = 'nvidia');
+	(X = 'amd' , Y = 'amd');
+    (X = 'amd', Y = 'intel')).
 
 compare_processors(P,Q):-
     A = 1, B = 2, B>A,
     processor_model(P,R),
     processor_model(Q,S),
     X = R, Y= S,
-    ((X = "i5", Y = "i3");
-    (X = "i7", Y = "i3");
-    (X = "i5", Y = "i5");
-    (X = "i3", Y = "i3");
-    (X = "i7", Y = "i7");
-    (X = "i7", Y = "i5")).
+    ((X = 'i5', Y = 'i3');
+    (X = 'i7', Y = 'i3');
+    (X = 'i5', Y = 'i5');
+    (X = 'i3', Y = 'i3');
+    (X = 'i7', Y = 'i7');
+    (X = 'i7', Y = 'i5')).
 
 compare_laptop(X,Y):-
 	compare_price(X,Y);
@@ -109,13 +106,13 @@ compare_laptop(X,Y):-
     compare_processors(X,Y);
     compare_gc_size(X,Y);
     compare_screen_size(X,Y).
-	
 
-	
+
+
 bubble_sort(List,Sorted):-b_sort(List,[],Sorted).
 b_sort([],Acc,Acc).
 b_sort([H|T],Acc,Sorted):-bubble(H,T,NT,Max),b_sort(NT,[Max|Acc],Sorted).
-   
+
 bubble(X,[],[],X).
 bubble(X,[Y|T],[Y|NT],Max):-compare_laptop(Y,X),bubble(X,T,NT,Max).
 bubble(X,[Y|T],[X|NT],Max):-compare_laptop(X,Y),bubble(Y,T,NT,Max).
@@ -139,4 +136,3 @@ chik([_|T],P,Result) :-
 input(P,M):-
     l(170,L),
     chik(L,P,M).
- 
