0990c75f05dabc18c69fb585960f3dc2d5ec921e
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2017-11-11 12:59:15 +0530

    Update expert.pl

--- expert.pl
+++ expert.pl
@@ -1,27 +1,9 @@
 printlaptop(H):-
     brand(H,A),
     model(H,B),
-    ram(H,C),
-    hd_type(H,D),
-    hd_size(H,E),
-    screen_size(H,F),
-    price(H,G),
-    processor_brand(H,I),
-    processor_model(H,J),
-    clock_speed(H,K),
-    gc_brand(H,L),
-    gc_size(H,M),
-    os(H,N),
-    weight(H,O),
-    write(A),
-    write(' '),
-    write(B),
-    write(' '),write(C),write(' '),write(D),
-    write(' '),write(E),write(' '),
-    write(F),write(' '),write(G),write(' '),
-    write(I),write(' '),write(J),write(' '),
-    write(K),write(' '),write(L),
-    write(' '),write(M),write(' '),write(N),write(' '),write(O),put(10).
+    write(H),write(' '),
+    write(A), write(' '),
+    write(B),nl.
 
 check([],_,[]).
 check([H|T],S,[H|Result]) :-
@@ -49,17 +31,19 @@ check([H|T],S,[H|Result]) :-
 
 check([_|T],S,Result) :-
     check(T,S,Result).
-	
-	
-test(M) :-
+
+
+test() :-
     write('Type of laptop: 1 for gaming, 2 for dev, 3 for clerk, 4 for home '),
+    nl,
     read(B),
     integer(B),
     write('Input Upper bound on Price '),
+    nl,
     read(P),
     integer(P),
     P>0,
     input(P,L),
 	bubble_sort(L,X),
     %numlist(1,168,L),
-	check(L,B,M).
+	check(X,B,M).
